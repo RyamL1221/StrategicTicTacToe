ac8881cad504234311189d8c10b908269e5781b1
RyamL1221 <ryaml1221@gmail.com>
2024-02-23 03:35:54 -0500

    added GridSizeException and implementation in SmallGrid class

---
+++ StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/exception/GridSizeException.java
@@ -0,0 +1,11 @@
+package main.java.StrategicTicTacToe.exception;
+
+public class GridSizeException extends RuntimeException{
+    
+    public GridSizeException() {
+        throw new RuntimeException("The grid size dimensions are not valid.");
+    }
+    public GridSizeException(String message) {
+        throw new RuntimeException(message);
+    }
+}
--- StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/model/SmallGrid.java
+++ StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/model/SmallGrid.java
@@ -1,11 +1,18 @@
 package main.java.StrategicTicTacToe.model;
 
+import main.java.StrategicTicTacToe.exception.GridSizeException;
+
 public class SmallGrid {
     private char[][] grid;
 
-    public SmallGrid() {}
+    public SmallGrid() {
+        this.grid = new char[3][3];
+    }
 
     public SmallGrids(char[][] grid) {
+        if(grid.length != 3 || grid[0].length != 3 || grid[1].length != 3 || grid[2].length != 3) {
+            throw new GridSizeException();
+        }
         this.grid = grid;
     }
 
@@ -19,5 +26,16 @@ public class SmallGrid {
 
     public String toString() {
         return "Grid: " + grid.toString();
+    }
+
+    public boolean gameOver() {
+        if(grid[0][0] == 'X' && grid[0][1] == 'X' && grid[0][2] == 'X' || grid[0][0] == 'O' && grid[0][1] == 'O' && grid[0][2] == 'O') { // top row
+            return true;
+        } else if(grid[0][0] == 'X' && grid[0][1] == 'X' && grid[0][2] == 'X' || grid[0][0] == 'O' && grid[0][1] == 'O' && grid[0][2] == 'O') {
+            return true;
+        } else {
+            return false;
+        }
+         
     }
 }
