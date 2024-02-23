18f2a9d281241ac75b9358e708c573ae515e7c70
RyamL1221 <ryaml1221@gmail.com>
2024-02-22 20:51:29 -0500

    SmallGrid class

--- StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/model/LargeGrid.java
+++ StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/model/LargeGrid.java
@@ -1,5 +1,5 @@
 package main.java.StrategicTicTacToe.model;
 
 public class LargeGrid {
-    
+    private SmallGrid[][] grid;
 }
--- StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/model/SmallGrid.java
+++ StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/model/SmallGrid.java
@@ -1,5 +1,23 @@
 package main.java.StrategicTicTacToe.model;
 
 public class SmallGrid {
-    
+    private char[][] grid;
+
+    public SmallGrid() {}
+
+    public SmallGrids(char[][] grid) {
+        this.grid = grid;
+    }
+
+    public char[][] getGrid() {
+        return grid;
+    }
+
+    public void setGrid(char[][] grid) {
+        this.grid = grid;
+    }
+
+    public String toString() {
+        return "Grid: " + grid.toString();
+    }
 }
