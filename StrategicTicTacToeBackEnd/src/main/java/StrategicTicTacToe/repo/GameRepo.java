cb2afa5c96504990f74d819c6940316e0cf433e3
RyamL1221 <RyamL1221@gmail.com>
2024-04-11 20:09:37 -0400

    implemented repository

---
+++ StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/repo/GameRepo.java
@@ -0,0 +1,14 @@
+package main.java.StrategicTicTacToe.repo;
+
+import org.springframework.stereotype.Repository;
+import java.util.*;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import StrategicTicTacToe.model.Game;
+
+@Repository
+public interface GameRepo extends MongoRepository<Game, Long>{
+    
+    Optional<Game> findGameById(String id);
+    void deleteGameById(String Id);
+
+}
