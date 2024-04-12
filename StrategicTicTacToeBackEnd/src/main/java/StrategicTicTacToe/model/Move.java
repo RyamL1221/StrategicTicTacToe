50db7a92f01b4c84e19240b49854ccd803060fe2
RyamL1221 <RyamL1221@gmail.com>
2024-04-12 00:17:43 -0400

    implemented Move class

--- StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/model/Move.java
+++ StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/model/Move.java
@@ -1,5 +1,41 @@
 package StrategicTicTacToe.model;
 
 public class Move {
-    
+    private Coordinate coordinate;
+    private char player;
+
+    public Move() {}
+
+    public Move(Coordinate coordinate, char player) {
+        this.coordinate = coordinate;
+        this.player = player;
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public char getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(char player) {
+        this.player = player;
+    }
+
+    public boolean equals(Move move) {
+        if(coordinate.equals(coordinate) && player == move.getPlayer()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "Coordinate: " + coordinate + "\tPlayer: " + player;
+    }
 }
