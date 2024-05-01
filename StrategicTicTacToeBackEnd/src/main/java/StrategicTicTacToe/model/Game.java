55d67cfb2b96cae79491fc27fb0cdd91ccd61fc6
RyamL1221 <ryaml1221@gmail.com>
2024-05-01 15:12:12 -0400

    Methods for Game.java

--- StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/model/Game.java
+++ StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/model/Game.java
@@ -15,4 +15,41 @@ public class Game {
     private String id;
     private LargeGrid largeGrid;
     private ArrayList<Move> moves;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LargeGrid getLargeGrid() {
+        return largeGrid;
+    }
+
+    public void setLargeGrid(LargeGrid largeGrid) {
+        this.largeGrid = largeGrid;
+    }
+
+    public ArrayList<Move> getMoves() {
+        return moves;
+    }
+
+    public void setMoves(ArrayList<Move> moves) {
+        this.moves = moves;
+    }
+
+    public void addMove(Move move) {
+        this.moves.add(move);
+    }
+
+    public void removeMove(Move move) {
+        this.moves.remove(move);
+    }
+
+    public String toString() {
+        return "Id: " + id;
+    }
+
 }
