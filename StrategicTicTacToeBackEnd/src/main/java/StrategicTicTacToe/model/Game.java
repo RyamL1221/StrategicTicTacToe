e493b04b368c4f368b362218028e43ce21058f53
RyamL1221 <ryaml1221@gmail.com>
2024-05-01 15:06:34 -0400

    created a moves instance variable in game to keep track of move history

--- StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/model/Game.java
+++ StrategicTicTacToeBackEnd/src/main/java/StrategicTicTacToe/model/Game.java
@@ -6,10 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.ArrayList;
+
 @Document(collection = "game")
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private String id;
     private LargeGrid largeGrid;
+    private ArrayList<Move> moves;
 }
